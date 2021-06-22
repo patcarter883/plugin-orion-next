be27b979737a676ba7cfe07c51c34419e0e13f99
Pat Carter <patcarter@agripath.com.au>
2021-06-22 15:44:07 +1000

    Composite key find

--- src/mixins/Repository.ts
+++ src/mixins/Repository.ts
@@ -2,6 +2,11 @@ import { Repository, Model, Element, Item, Collection } from '@vuex-orm/core'
 import { FilterOperator } from '@tailflow/laravel-orion/lib/drivers/default/enums/filterOperator'
 import { Model as OrionModel } from '@tailflow/laravel-orion/lib/model'
 
+export interface iQueryFunctions<M extends Model> {
+  lookFor(string: string): Promise<Item<M> | Collection<M>>
+  scope(scope: string)
+}
+
 export function mixin(repository: typeof Repository): void {
   const repo = repository.prototype as Repository<Model>
 
@@ -54,10 +59,14 @@ export function mixin(repository: typeof Repository): void {
     const orion = model.$self().orionModel as OrionModel
     let orionResponse
     if (Array.isArray(ids)) {
-      orionResponse = await orion
-        .$query()
-        .filter('id', FilterOperator.In, ids)
-        .search()
+      const pk = model.$primaryKey() as string[]
+      const query = orion.$query()
+
+      pk.forEach((value, index) => {
+        query.filter(value, FilterOperator.Equal, ids[index])
+      })
+
+      orionResponse = await query.search()
       this.save(orionResponse.map((m) => m.$attributes))
       return this.query().find(ids)
     } else {
--- src/types/vuex-orm.ts
+++ src/types/vuex-orm.ts
@@ -1,25 +1,19 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import { Model, Element, Item, Collection } from '@vuex-orm/core'
+import { iQueryFunctions } from 'src/mixins/Repository'
 
 declare module '@vuex-orm/core/dist/src/repository/Repository' {
   export interface Repository<M extends Model = Model> {
     $save(records: Element | Element[]): Promise<M | M[]>
-    // $save(records: Element[]): Promise<M[]>
-    // $save(record: Element): Promise<M>
-    // $update(records: Element[]): Promise<M[]>
-    // $update(record: Element): Promise<M>
     $update(records: Element | Element[]): Promise<M | M[]>
-    // $find(id: string | number): Promise<Item<M>>
-    // $find(ids: (string | number)[]): Promise<Collection<M>>
     $find(
       ids: (string | number) | (string | number)[]
     ): Promise<Item<M> | Collection<M>>
     $all(): Promise<Collection<M>>
-    // $destroy(ids: (string | number)[]): Promise<Collection<M>>
-    // $destroy(id: string | number): Promise<Item<M>>
     $destroy(
       ids: (string | number) | (string | number)[]
     ): Promise<Item<M> | Collection<M>>
+    $query: iQueryFunctions<M>
   }
 }
 
